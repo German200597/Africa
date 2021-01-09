a0a8682d57082ebfbe8ca6c22431d7284086d0cf
Герман <german@MacBook-Air-German.local>
2021-01-09 13:59:07 +0300

    converted into a function

--- main_two.py
+++ main_two.py
@@ -9,22 +9,24 @@ root = tree.getroot()
 new_data = root.findall('channel/item')
 # print(len(new_data))
 counter = {}
-for new in new_data:
-    finder = new.find('description')
-    text = finder.text
-    news=list(text.split())
-    count=0
-    for word in news: 
-        if len(word)>6:
-            if word in counter:
-                count+=1
-                counter[word] = int(count)
-            else:
-                count=1
-                counter[word] = count
-result=sorted(counter.items(), key=operator.itemgetter(1), reverse=True) 
-count=0 
-for i in result:
-    count+=1
-    if count<=10:
-        print(i[0])
+def sorter():
+    for new in new_data:
+        finder = new.find('description')
+        text = finder.text
+        news=list(text.split())
+        count=0
+        for word in news: 
+            if len(word)>6:
+                if word in counter:
+                    count+=1
+                    counter[word] = int(count)
+                else:
+                    count=1
+                    counter[word] = count
+    result=sorted(counter.items(), key=operator.itemgetter(1), reverse=True) 
+    count=0 
+    for i in result:
+        count+=1
+        if count<=10:
+            print(i[0])
+sorter()            

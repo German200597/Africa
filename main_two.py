e421bfa2ac2bc6b5a6f8e568088dfe02b0e86948
Герман <german@MacBook-Air-German.local>
2021-01-13 15:54:33 +0300

    parameters added

--- main_two.py
+++ main_two.py
@@ -1,16 +1,18 @@
 from pprint import pprint
 import operator
 import xml.etree.ElementTree as ET
-def reader():
+def reader(address):
     parser = ET.XMLParser(encoding='utf-8')
     tree=ET.parse('newsafr.xml', parser) 
     root = tree.getroot()
-    global new_data
     new_data = root.findall('channel/item')
-# print(len(new_data))
-def sorter():
+    return new_data
+
+data_from_file=reader('newsafr.json')    
+
+def sorter(data_from_file, number):
     counter = {}
-    for new in new_data:
+    for new in data_from_file:
         finder = new.find('description')
         text = finder.text
         news=list(text.split())
@@ -22,15 +24,19 @@ def sorter():
                     counter[word] = int(count)
                 else:
                     count=1
-                    counter[word] = count
-    global result                
+                    counter[word] = count                
     result=sorted(counter.items(), key=operator.itemgetter(1), reverse=True) 
-def top():    
+    return result
+
+data_from_file_two=sorter(data_from_file, 6)
+
+def top(data_from_file_two):    
     count=0 
-    for i in result:
+    for i in data_from_file_two:
         count+=1
         if count<=10:
-            print(i[0])
-reader()            
-sorter()    
-top()               
+            return i[0]
+
+reader('newsafr.json')            
+sorter(data_from_file, 6)    
+top(data_from_file_two)               

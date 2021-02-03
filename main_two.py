21006e1fa94b227de40ec43607821d421d0ad74f
Герман <german@MacBook-Air-German.local>
2021-02-03 17:56:00 +0300

    function edited

--- main_two.py
+++ main_two.py
@@ -3,40 +3,38 @@ import operator
 import xml.etree.ElementTree as ET
 def reader(address):
     parser = ET.XMLParser(encoding='utf-8')
-    tree=ET.parse('newsafr.xml', parser) 
+    tree = ET.parse(address, parser) 
     root = tree.getroot()
     new_data = root.findall('channel/item')
     return new_data
 
-data_from_file=reader('newsafr.json')    
+data_from_file = reader('newsafr.json')
 
-def sorter(data_from_file, number):
+def sorter(data_from_file, max_number_of_letters):
     counter = {}
     for new in data_from_file:
         finder = new.find('description')
         text = finder.text
-        news=list(text.split())
-        count=0
+        news = list(text.split())
+        count = 0
         for word in news: 
-            if len(word)>6:
+            if len(word) > number_of_letters:
                 if word in counter:
-                    count+=1
-                    counter[word] = int(count)
+                    counter[word] = counter[word] + 1
                 else:
-                    count=1
-                    counter[word] = count                
+                    counter[word] = 1               
     result=sorted(counter.items(), key=operator.itemgetter(1), reverse=True) 
     return result
 
 data_from_file_two=sorter(data_from_file, 6)
 
-def top(data_from_file_two):    
-    count=0 
+def top(data_from_file_two, number_of_top_words):    
+    count = 0 
     for i in data_from_file_two:
-        count+=1
-        if count<=10:
-            return i[0]
+        count += 1
+        if count <= number_of_top_words:
+            print(i[0])
 
 reader('newsafr.json')            
 sorter(data_from_file, 6)    
-top(data_from_file_two)               
+top(data_from_file_two, 10)               
